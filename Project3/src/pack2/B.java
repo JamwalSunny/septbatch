a8da125a9c7acc82520b648f3cfcd60f94924d22
JamwalSunny <sjamwal-c@beesolvertechnology.com>
2024-10-20 16:15:13 +0530

    pushing the code first time

---
+++ Project3/src/pack2/B.java
@@ -0,0 +1,16 @@
+package pack2;
+
+public class B {
+	public void display()
+    {
+   	 System.out.println("Hello from java");
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B ob = new B();
+		ob.display();
+
+
+	}
+
+}
